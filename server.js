b5c1ec5831add4a1fa569e487770e0b7c772c7bc
Carla Brady <carlakaybrady@gmail.com>
2018-02-27 15:23:50 -0600

    removed commented code

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-// var indexRouter = require('./routes/index.Router')
 var empRouter = require('./routes/employee.router');
 var mgrRouter = require('./routes/manager.router');
 
@@ -11,7 +10,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/employee', empRouter);
 app.use('/manager', mgrRouter);
-// app.use('/', indexRouter);
 
 app.listen(5005, function() {
   console.log('up on 5005');
